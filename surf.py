17740d1626c1c2e3e9f59f76ea440bf74093a9c1
oindree <banerjee.104@osu.edu>
2015-06-30 13:34:08 -0400

    improved LED on off release functions

--- surf.py
+++ surf.py
@@ -266,27 +266,31 @@ class SURF(ocpci.Device):
         self.write(self.map['spi_cs'], int(val))
 		
 		
-	def led(self):
-		self.led = bf(self.read(self.map['SURF_LED']))
-		if 1 is in self.led[11:0]:
-			led_off("off")                      # call the function for turning LED's off 
-		if 0 is in self.led[11:0]:
-			led_on("on")                        # call the function for turning LED's on 
-		if f is in self.led[27:16]:
-			led_release("release")              # call function for releasing LED whatever that means 
-			
+def led(self, *arg, **kwargs):
+	self.led = bf(self.read(self.map['SURF_LED']))
+	if arg == "off":
+		led_off()                        # call the function for turning LED's off 
+	if arg == "on":
+		led_on()                         # call the function for turning LED's on 
+	if arg == "release":
+		led_release()                    # call function for releasing LED (we stop controlling it)
+		
 
-	def led_off(self, "off"):
-		for i in range len(self.led[11:0]):
-			self.led[i] = 0 
-			
-	def led_on(self, "on"):
-		for i in range len(self.led[11:0]):
-			self.led[i] = 1
+def led_off(self):
+	for i in range len(self.led[27:16]):
+		self.led[i] = 1 
+	for j in range len(self.led[11:0]):
+		self.led[j] = 0 
+		
+def led_on(self):
+	for i in range len(self.led[27:16]):
+		self.led[i] = 1
+	for j in range len(self.led[11:0]):
+		self.lef[j] = 1 
 
-	def led_release(self, "release"):
-		for i in range len(self.led[27:16]):
-			self.led[i] = 0 
+def led_release(self):
+	for i in range len(self.led[27:16]):
+		self.led[i] = 0 
 			
 		
 
