ebbaae8b7ac8bb3380689c0717bf5313c3ffce77
ejobe <ejo@uchicago.edu>
2018-07-23 11:55:50 -0400

    update readme, add setup scripts

---
+++ setup_pci_uio.sh
@@ -0,0 +1,19 @@
+echo "  "
+echo "  "
+echo "Setting up the PCI..."
+echo "  "
+sudo modprobe uio_pci_generic
+echo "  " 
+sudo bash -c "echo 10ee ff01 > /sys/bus/pci/drivers/uio_pci_generic/new_id"
+echo "10ee f01 has been assigned the uio_pci_generic driver"
+sudo bash -c "echo 10ee ff00 > /sys/bus/pci/drivers/uio_pci_generic/new_id"
+echo "10ee f00 has been assigned the uio_pci_generic driver"
+sudo chmod 666 /sys/class/uio/uio0/device/resource0
+echo "permission changed on resource0"
+sudo chmod 666 /sys/class/uio/uio0/device/resource1
+echo "permission changed on resource 1"
+echo "  "
+echo "  "
+echo "PCI set-up complete" 
+echo "  "
+echo "  "
